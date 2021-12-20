035498368f9706b58c88a8d05f81fa3f76fe160f
ejkitchen <ejkitchen@gmail.com>
2021-12-19 21:40:18 -0500

    Added progress bar, made test files larger, added checks for arguments, cleaned up code, error checking etc

--- cleans.py
+++ cleans.py
@@ -1,3 +1,7 @@
+"""
+Yara log file cleanser
+"""
+
 import sys
 import getopt
 import shutil
@@ -6,20 +10,24 @@ import glob
 import os
 import errno
 from pathlib import Path
+import datetime
+from tqdm import tqdm
+import humanize
+from collections import defaultdict
 
+# this is a pointer to the module object instance itself.
+this = sys.modules[__name__]
+this.matchesdict = defaultdict(list)
 
 class RequiredOptions:
     def __init__(self, options_long=[]):
-
         self.required_options = options_long
 
     def add(self, option):
-
         if option not in self.required_options:
             self.required_options.append(option)
 
     def resolve(self, option):
-
         if option in self.required_options:
             self.required_options.remove(option)
 
@@ -90,25 +98,26 @@ def get_args(argv):
             if (not os.path.isfile(yara_file)):
                 print(f"Cannot open Yara rules file: {yara_file}")
                 sys.exit(3)
-            required_options.resolve("yara=")
+            required_options.resolve(yarafile_option)
         elif opt in ("-f", "--folder"):
             input_folder = arg
             if (not os.path.isdir(input_folder)):
                 print(f"Directory to cleans not found: {input_folder}")
                 sys.exit(4)
-            required_options.resolve("folder=")
+            required_options.resolve(folder_option)
         elif opt in ("-o", "--output"):
             output_folder = arg
-            try:
-                Path.mkdir(output_folder, parents=True, exist_ok=False)
-            except OSError as e:
-                if e.errno == errno.EEXIST:
+            if (os.path.exists(output_folder)):
+                if (os.path.isdir(output_folder)):
                     print(
                         f"Directory {output_folder} already exists. You must choose another directory or remove it")
+                    sys.exit(6)
                 else:
-                    print(f"os error: {e.errno}")
-                    sys.exit(5)
-            required_options.resolve("output=")
+                    print(
+                        f"Target {output_folder} is a file. Please specify a directory")
+                    sys.exit(7)
+
+            required_options.resolve(output_option)
 
     if not required_options.optionsResolved():
         print(
@@ -119,16 +128,12 @@ def get_args(argv):
         usage()
         sys.exit(1)
 
-    sys.exit(0)
-
-    print('   Yara rules: ', yara_file)
-    print(' Input folder:', input_folder)
-    print('Output folder:', input_folder)
-
-    return yara_file, input_folder
+    return yara_file, input_folder, output_folder
 
 
-def scandir(dir):
+def scandir(dir, totalsizein):
+    totalsize = 0
+
     subfolders, files = [], []
 
     for f in os.scandir(dir):
@@ -136,12 +141,15 @@ def scandir(dir):
             subfolders.append(f.path)
         if f.is_file():
             files.append(f.path)
+            totalsize += os.path.getsize(f.path)
 
     for dir in list(subfolders):
-        sf, f = scandir(dir)
+        sf, f, ts = scandir(dir, totalsize)
+        totalsize+=ts
         subfolders.extend(sf)
         files.extend(f)
-    return subfolders, files
+
+    return subfolders, files, totalsize
 
 
 def get_replacement_string(yara_file):
@@ -160,16 +168,36 @@ def get_replacement_string(yara_file):
     return result
 
 
-def process_folder(yara_file, input_folder, output_folder):
-    _, files = scandir(input_folder)
+def time_log_print(message):
+    print(f"{datetime.datetime.now():%Y-%m-%d @ %H:%M:%S} - {message}")
 
+
+def process_folder(yara_file, input_folder, output_folder):
+    time_log_print("Scanning directories...")
+
+    subfolders, files, totalsize = scandir(input_folder, 0)
+
+    time_log_print(f"Done.")
+    time_log_print(f"{len(subfolders)} directories scanned")
+    time_log_print(f"{len(files)} files found")
+    time_log_print(f"Total size: {humanize.naturalsize(totalsize)} - {humanize.intcomma(totalsize)} bytes")
+    
+
+    time_log_print(f"Compiling yara file...")
+    # todo: catch errors in yara file, right now just throws an exception
     rule = yara.compile(yara_file)
+    time_log_print(f"Done.")
 
     replacement = get_replacement_string(yara_file)
 
+    time_log_print(f"Cleansing files...")
+
     for file in files:
-        print("\n\n", file)
         output_file_path = output_folder + file[1:]
+
+        #if "chrome.dmg" in output_file_path.lower():
+        #    print("Here")
+
         matches = rule.match(file)
 
         # Read in the file
@@ -179,9 +207,13 @@ def process_folder(yara_file, input_folder, output_folder):
 
         for match in matches:
             print(file.name, "has", len(match.strings), "matches")
-            for item in match.strings:
+            this.matchesdict[file.name] = match.strings
+
+            for item in tqdm(match.strings):
                 (original_offset, rule_name, text) = item
+                #time_log_print(f"{item}")
                 filedata = filedata.replace(text, replacement)
+                
 
         # create folders if not exists
         if not os.path.exists(os.path.dirname(output_file_path)):
@@ -194,13 +226,23 @@ def process_folder(yara_file, input_folder, output_folder):
         # Write the replaced output
         with open(output_file_path, 'wb') as file:
             file.write(filedata)
-            print("\nnew file created at", output_file_path)
+            #print("\nnew file created at", output_file_path)
+
+    time_log_print(f"Done.")
 
 
 def main(argv):
-    yara_file, input_folder = get_args(argv)
 
-    process_folder(yara_file, input_folder)
+    yara_file, input_folder, output_folder = get_args(argv)
+
+    print("==============")
+    print("   Yara rules: ", yara_file)
+    print(" Input folder: ", input_folder)
+    print("Output folder: ", output_folder)
+    print("==============")
+    print("")
+
+    process_folder(yara_file, input_folder, output_folder)
 
 
 if __name__ == "__main__":
